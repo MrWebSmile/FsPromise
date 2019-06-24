e8f2902180ad539bd8203268017882601622d053
MrWebSmile <MrWebSmile@users.noreply.github.com>
2019-06-24 09:55:44 +0200

    Bibliothèque fs promise

---
+++ fs-promise.js
@@ -0,0 +1,17 @@
+const fs = require('fs')
+var fsPromise = function(){
+
+}
+
+fsPromise.prototype.read = function(fic){
+    return new Promise(function(resolve,reject){
+        fs.readFile(fic,'utf8', (err, data) => {
+            if (err) reject(err);
+            //console.log(data);
+            dataJson = JSON.parse(data);
+            resolve(dataJson);
+          })
+    });
+}
+
+module.exports = {fs_promise:fsPromise};
